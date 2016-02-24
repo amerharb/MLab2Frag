8e9215b432299649def2e13bea70636f5554ad2e
amerharb <amer.harb@gmail.com>
2016-02-24 19:55:18 +0100

    add list frag and other files were forgeten from first commit

---
+++ app/src/main/java/digitalalpha/mlab2frag/ListFragment.java
@@ -0,0 +1,19 @@
+package digitalalpha.mlab2frag;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_list, container, false);
+
+    }
+}
+
