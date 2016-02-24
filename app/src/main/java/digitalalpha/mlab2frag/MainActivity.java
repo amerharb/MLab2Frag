3dc6bc9c433d75497c736c7925bd87507c9a374c
amerharb <amer.harb@gmail.com>
2016-02-24 19:22:18 +0100

    first init so far the contact frag and about frag is only apear in the main activity

--- app/src/main/java/digitalalpha/mlab2frag/MainActivity.java
+++ app/src/main/java/digitalalpha/mlab2frag/MainActivity.java
@@ -1,13 +1,42 @@
 package digitalalpha.mlab2frag;
 
+import android.content.Context;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    ContactFragment cFrag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //AboutFragment aboutFragment = new AboutFragment();
+        //ContactFragment contactFragment = new ContactFragment();
+
+        //FragmentManager fm = getSupportFragmentManager();
+        //FragmentTransaction ft = fm.beginTransaction();
+
+        //ft.add(R.id.fContact, contactFragment);
+        //ft.commit();
+
+        //ft.add(R.id.fAbout,aboutFragment);
+        //ft.commit();
+
+
     }
+
+    @Override
+    public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
+        return super.onCreateView(parent, name, context, attrs);
+
+    }
+
+
 }
